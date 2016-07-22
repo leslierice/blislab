929325ceea3fb1069b6015fa7a82909148339565
Jianyu Huang <jianyu@cs.utexas.edu>
2016-07-22 11:28:13 -0500

    Fix the issue with TACC SLURM script for parallel performance

--- step6/test/tacc_run_bl_dgemm.sh
+++ step6/test/tacc_run_bl_dgemm.sh
@@ -7,7 +7,7 @@
 #SBATCH -N 1
 #SBATCH -A CompEdu 
 export OMP_NUM_THREADS=8
-export BLISGEMM_IC_NT=8
+export BLISLAB_IC_NT=8
 export KMP_AFFINITY=compact,verbose
 
 ibrun tacc_affinity run_bl_dgemm.sh
